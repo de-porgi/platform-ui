6a7ba7023272ce46c37ddb18c84034b4a20086f4
Nhu Viet Nguyen <braveryandglory@gmail.com>
2021-01-26 11:32:11 +0200

    React-router-dom and ApplicationForm in components dir

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import React from 'react'
-import { Main } from '@aragon/ui'
+import { 
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch
+} from 'react-router-dom'
+import { 
+  Main,
+  Header,
+  Button,
+  Tabs, 
+} from '@aragon/ui'
+import ApplicationForm from './components/ApplicationForm'
 
 function App() {
   return (
     <Main>
-      <h1>Hello PORGI!</h1>
+      <Router>
+        <Header
+        primary="PORGI"
+        secondary={
+          <Button mode="strong"><Link to="/form">Apply your idea!</Link></Button>
+        }
+        />
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Active projects</Link>
+          </li>
+          <li>
+            <Link to="/le">Pre-sale projects</Link>
+          </li>
+        </ul>
+        <Switch>
+        <Route exact path="/">
+            PORGI
+          </Route>
+          <Route path="/form">
+            <ApplicationForm />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
     </Main>
   )
 }
---
+++ src/components/ApplicationForm.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {
+  Box,
+  Button,
+  GU,
+  Header,
+  IconMinus,
+  IconPlus,
+  Main,
+  SyncIndicator,
+  Tabs,
+  textStyle,
+} from '@aragon/ui'
+
+function ApplicationForm() {
+    return (
+    <Main>
+      <div>Your app goes here</div>
+    </Main>
+    );
+}
+
+export default ApplicationForm
