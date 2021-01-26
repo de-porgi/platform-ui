2ab87d3bd148654c4301a80cfbf49d309555fb84
Nhu Viet Nguyen <braveryandglory@gmail.com>
2021-01-26 16:41:03 +0200

    applied prettifier for .js files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React from 'react'
-import { 
-  BrowserRouter as Router,
-  Link,
-  Route,
-  Switch
-} from 'react-router-dom'
-import { 
-  Main,
-  Header,
-  Button,
-  Tabs, 
-} from '@aragon/ui'
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
+import { Main, Header, Button, Tabs } from '@aragon/ui'
 import ApplicationForm from './components/ApplicationForm'
 
 function App() {
@@ -18,30 +8,32 @@ function App() {
     <Main>
       <Router>
         <Header
-        primary="PORGI"
-        secondary={
-          <Button mode="strong"><Link to="/form">Apply your idea!</Link></Button>
-        }
+          primary="PORGI"
+          secondary={
+            <Button mode="strong">
+              <Link to="/form">Apply your idea!</Link>
+            </Button>
+          }
         />
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Active projects</Link>
-          </li>
-          <li>
-            <Link to="/le">Pre-sale projects</Link>
-          </li>
-        </ul>
-        <Switch>
-        <Route exact path="/">
-            PORGI
-          </Route>
-          <Route path="/form">
-            <ApplicationForm />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Active projects</Link>
+            </li>
+            <li>
+              <Link to="/le">Pre-sale projects</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route exact path="/">
+              PORGI
+            </Route>
+            <Route path="/form">
+              <ApplicationForm />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </Main>
   )
 }
