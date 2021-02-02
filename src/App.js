3f0696613eb080e071f0a2cc5137ee90f1fd1cf4
Wondertan <hlibwondertan@gmail.com>
2021-02-02 15:34:22 +0200

    define app routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import { Main, Header } from '@aragon/ui'
 import { Spring, animated } from 'react-spring'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import AccountModule from './components/AccountModule/AccountModule'
+import { Landing } from './routes/Landing'
+import { Projects } from './routes/Projects'
+import { NewProject } from './routes/NewProject'
+import { Account } from './routes/Account'
+import { Project } from './routes/Project'
+
+const Router = BrowserRouter
 
 function App() {
   return (
@@ -20,6 +28,15 @@ function App() {
           >
             <Main>
               <Header primary="Hello Porgi" secondary={<AccountModule />} />
+              <Router>
+                <Switch>
+                  <Route exact path="/" component={Landing} />
+                  <Route path="/projects" component={Projects} />
+                  <Route path="/new" component={NewProject} />
+                  <Route path="/account" component={Account} />
+                  <Route path="/project/:project" component={Project} />
+                </Switch>
+              </Router>
             </Main>
           </animated.div>
         </animated.div>
